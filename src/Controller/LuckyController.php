ff4a5ab51efc315b09d7e44ad356345e06cedad4
Arnaud Solier <arnaud.s@objectif54.fr>
2022-10-11 11:57:50 +0200

    création d'un fichier twig + import de la fonction number dedans

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -3,25 +3,21 @@
 // src/Controller/LuckyController.php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class LuckyController
+class LuckyController extends AbstractController
 {
-    #[Route('/lucky/number')]
+    /**
+     * @Route("/lucky/number")
+     */
     public function number(): Response
     {
         $number = random_int(0, 100);
 
-        return new Response(
-        '
-        <html>
-        <body>
-            <h1>Générateur de nombre de 1 à 100</h1>
-            <h2>Lucky number: '.$number.'</h2>
-        </body>
-        </html>
-        '
-        );
+        return $this->render('lucky/number.html.twig',[
+            'number' => $number,
+        ]);
     }
 }
